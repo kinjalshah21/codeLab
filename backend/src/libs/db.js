66934da0817264edafc4baa55380a13eb693af16
Kinjal_Shah <kinjalshah83415@gmail.com>
2025-04-21 01:38:05 +0530

    user schema migration done

---
+++ backend/src/libs/db.js
@@ -0,0 +1,12 @@
+import { PrismaClient } from "../generated/prisma/index.js";
+
+const globalForPrisma = globalThis;
+
+const db = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+    globalForPrisma.prisma = db;
+}
+
+export {db};
+
