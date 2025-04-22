a9e6c488bfa249c4b489217f47b84d7ebbbfdc71
Kinjal_Shah <kinjalshah83415@gmail.com>
2025-04-23 00:42:21 +0530

    user registration controller added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,14 +1,65 @@
 import bcrypt from "bcryptjs";
+import { db } from "../libs/db.js";
+import { UserRole } from "../generated/prisma/index.js";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
+	const { name, email, password } = req.body;
 
-}
-export const login = async (req, res) => {
+	try {
+		const existingUser = await db.user.findUnique({
+			where: {
+				email,
+			},
+		});
 
-}
-export const logout = async (req, res) => {
+		if (existingUser) {
+			return res.status(400).json({
+				error: "User already exists!",
+			});
+		}
 
-}
-export const me = async (req, res) => {
+		const hashPassword = await bcrypt.hash(password, 10);
 
-}
+		const newUser = await db.user.create({
+			data: {
+				email,
+				password: hashPassword,
+				name,
+				role: UserRole.USER,
+			},
+		});
+
+		const token = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET, {
+			expiresIn: "7d",
+		});
+
+		res.cookie(jwt, token, {
+			httpOnly: true,
+			sameSite: "strict",
+			secure: process.env.NODE_ENV !== "development",
+            maxAge : 1000 * 60 * 60 * 24 * 7
+		});
+
+        res.status(201).json({
+            success: true,
+            message: "User created successfully!",
+            user: {
+                id : newUser.Id,
+                email: newUser.email,
+                name: newUser.name,
+                role: newUser.role,
+                image: newUser.image // to be implemented
+            }
+        })
+	} catch (error) {
+        console.error('Error creating the user: ', error);
+        res.status(500).json({
+            success: false,
+            error: 'Error creating the user'
+        })
+    }
+};
+export const login = async (req, res) => {};
+export const logout = async (req, res) => {};
+export const me = async (req, res) => {};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config();
@@ -7,7 +8,7 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-
+app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
 app.get("/", (req, res) => {
