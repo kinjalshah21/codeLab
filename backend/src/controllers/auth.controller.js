69e1acae7b69bc16bf611f61621bb487cb15e416
Kinjal_Shah <kinjalshah83415@gmail.com>
2025-04-24 10:43:54 +0530

    authentication error fixed. login functionality added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -34,32 +34,87 @@ export const register = async (req, res) => {
 			expiresIn: "7d",
 		});
 
-		res.cookie(jwt, token, {
+		res.cookie("jwt-token", token, {
 			httpOnly: true,
 			sameSite: "strict",
 			secure: process.env.NODE_ENV !== "development",
-            maxAge : 1000 * 60 * 60 * 24 * 7
+			maxAge: 1000 * 60 * 60 * 24 * 7,
 		});
 
-        res.status(201).json({
-            success: true,
-            message: "User created successfully!",
-            user: {
-                id : newUser.Id,
-                email: newUser.email,
-                name: newUser.name,
-                role: newUser.role,
-                image: newUser.image // to be implemented
-            }
-        })
+		res.status(201).json({
+			success: true,
+			message: "User created successfully!",
+			user: {
+				id: newUser.Id,
+				email: newUser.email,
+				name: newUser.name,
+				role: newUser.role,
+				image: newUser.image, // to be implemented
+			},
+		});
 	} catch (error) {
-        console.error('Error creating the user: ', error);
-        res.status(500).json({
-            success: false,
-            error: 'Error creating the user'
-        })
-    }
+		console.error("Error creating the user: ", error);
+		res.status(500).json({
+			success: false,
+			error: "Error creating the user",
+		});
+	}
 };
-export const login = async (req, res) => {};
+export const login = async (req, res) => {
+	const { email, password } = req.body;
+
+	try {
+		const user = await db.user.findUnique({
+			where: {
+				email,
+			},
+		});
+
+		if (!user) {
+			return res.status(401).json({
+				success: false,
+				message: "User not found.",
+			});
+		}
+
+		const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+		if (!isPasswordMatch) {
+			res.status(401).json({
+				success: false,
+				message: "Invalid Credentials.",
+			});
+		}
+
+		const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+			expiresIn: "7d",
+		});
+
+		res.cookie("jwt-token", token, {
+			httpOnly: true,
+			sameSite: "strict",
+			secure: process.env.NODE_ENV !== "development",
+			maxAge: 1000 * 60 * 60 * 24 * 7,
+		});
+
+		res.status(200).json({
+			success: true,
+			message: "User Logged In successfully.",
+			user: {
+				id: user.Id,
+				email: user.email,
+				name: user.name,
+				role: user.role,
+				image: user.image,
+			},
+		});
+	} catch (error) {
+		console.error("Error logging in the user: ", error);
+		res.status(500).json({
+			success: false,
+			error: "Error logging in the user",
+		});
+	}
+};
 export const logout = async (req, res) => {};
 export const me = async (req, res) => {};
