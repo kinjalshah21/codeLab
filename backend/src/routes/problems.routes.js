861ef1564c18ef3d4466b1987f06d618107ed3e8
Kinjal_Shah <kinjalshah83415@gmail.com>
2025-05-03 15:15:58 +0530

    problem router setup done

--- backend/src/routes/problems.routes.js
+++ backend/src/routes/problems.routes.js
@@ -1,8 +1,20 @@
 import express from "express";
-import { authMiddleware } from "../middleware/auth.middleware.js";
+import { authMiddleware, checkAdmin } from "../middleware/auth.middleware.js";
+import { createProblem, deleteProblem, getAllProblems, getAllProblemsSolvedByUser, getProblemById, updateProblem } from "../controllers/problems.controller.js";
 
 const problemRoutes = express.Router();
 
-problemRoutes.post("/create-problem", authMiddleware);
+problemRoutes.post("/create-problem", authMiddleware, checkAdmin, createProblem);
+
+problemRoutes.post("/get-all-problems", authMiddleware, getAllProblems);
+
+problemRoutes.post("/get-problem/:id", authMiddleware, getProblemById);
+
+problemRoutes.put("/update-problem/:id", authMiddleware, checkAdmin, updateProblem);
+
+problemRoutes.delete("/delete-problem/:id", authMiddleware, checkAdmin, deleteProblem);
+
+problemRoutes.get("/get-solved-problems", authMiddleware, getAllProblemsSolvedByUser);
+
 
 export default problemRoutes;
