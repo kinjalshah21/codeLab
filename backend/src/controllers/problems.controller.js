11564b68da807ac7a9b2cca9cb78a711085f9c21
Kinjal_Shah <kinjalshah83415@gmail.com>
2025-05-04 12:13:08 +0530

    createProblem controller added

--- backend/src/controllers/problems.controller.js
+++ backend/src/controllers/problems.controller.js
@@ -1,20 +1,99 @@
+import { UserRole } from "../generated/prisma/index.js";
+import { db } from "../libs/db.js";
+import {
+	getJudge0LanguageId,
+	pollBatchResults,
+	submitBatch,
+} from "../libs/judge0.lib.js";
 
-export const createProblem = async(req, res) => {
+export const createProblem = async (req, res) => {
+	// get required data from request body
+	const {
+		title,
+		description,
+		difficulty,
+		tags,
+		examples,
+		constraints,
+		testcases,
+		codeSnippets,
+		referenceSolution,
+	} = req.body;
+	// check user role once again
 
-}
+	if (req.user.role !== UserRole.ADMIN) {
+		return res.status(403).json({
+			success: true,
+			message: "You are not allowed to create problem.",
+		});
+	}
+	// loop through each reference solution for different language.
 
-export const getAllProblems = async(req, res) => {
-  
-}
-export const getProblemById = async(req, res) => {
+	try {
+		for (const [language, solutionCode] of Object.entries(
+			referenceSolution
+		)) {
+			const languageId = getJudge0LanguageId(language);
 
-}
-export const updateProblem = async(req, res) => {
+			if (!languageId) {
+				return res.status(400).json({
+					success: false,
+					error: `Language ${language} not supported!`,
+				});
+			}
 
-}
-export const deleteProblem = async(req, res) => {
+			const submissions = testcases.map(({ input, output }) => ({
+				source_code: solutionCode,
+				language_id: languageId,
+				stdin: input,
+				expected_output: output,
+			}));
 
-}
-export const getAllProblemsSolvedByUser = async(req, res) => {
+			const submissionResults = await submitBatch(submissions);
 
-}
+			const tokens = submissionResults.map((res) => res.token);
+
+			const results = await pollBatchResults(tokens);
+
+			for (let i = 0; i < results.length; i++) {
+				const result = results[i];
+
+				if (result.status.id !== 3) {
+					return res.status(400).json({
+						success: false,
+						error: `Testcase ${
+							i + 1
+						} failed for language ${language}`,
+					});
+				}
+			}
+
+			//save the problem to the database
+
+			const newProblem = await db.problem.create({
+				title,
+				description,
+				difficulty,
+				tags,
+				examples,
+				constraints,
+				testcases,
+				codeSnippets,
+				referenceSolution,
+				userId: req.user.id,
+			});
+
+      return res.status(201).json(newProblem);
+		}
+	} catch (error) {}
+};
+
+export const getAllProblems = async (req, res) => {};
+
+export const getProblemById = async (req, res) => {};
+
+export const updateProblem = async (req, res) => {};
+
+export const deleteProblem = async (req, res) => {};
+
+export const getAllProblemsSolvedByUser = async (req, res) => {};
