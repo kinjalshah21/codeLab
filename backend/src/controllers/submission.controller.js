b548cf4710fcfc06642a618f4590f08db8636fea
Kinjal_Shah <kinjalshah83415@gmail.com>
2025-05-12 21:02:17 +0530

    submission routes added.

---
+++ backend/src/controllers/submission.controller.js
@@ -0,0 +1,76 @@
+import { db } from "../libs/db.js";
+
+export const getAllSubmission = async (req, res) => {
+	const userId = req.user.id;
+
+	try {
+		const submissions = await db.submission.findMany({
+			where: {
+				userId,
+			},
+		});
+
+		res.status(200).json({
+			success: true,
+			message: "Submissions fetched successfully!",
+			submissions,
+		});
+	} catch (error) {
+		console.error("Error in getting all the submissions ", error);
+		res.status(500).json({
+			success: false,
+			message: "Error in fetching submissions!",
+			error: error.message,
+		});
+	}
+};
+export const getSubmissionsForProblem = async (req, res) => {
+	const userId = req.user.Id;
+	const problemId = req.params;
+
+	try {
+		const submissions = await db.submission.findMany({
+			where: {
+				userId,
+				problemId,
+			},
+		});
+
+		res.status(200).json({
+			success: true,
+			message: "Submissions fetched successfully!",
+			submissions,
+		});
+	} catch (error) {
+		console.error("Error in getting all the submissions ", error);
+		res.status(500).json({
+			success: false,
+			message: "Error in fetching submissions!",
+			error: error.message,
+		});
+	}
+};
+export const getSubmissionCountForProblem = async (req, res) => {
+	const problemId = req.params;
+
+	try {
+		const submission = await db.submission.count({
+			where: {
+				problemId,
+			},
+		});
+
+		res.status(200).json({
+			success: true,
+			message: "Submissions count successfully!",
+			count: submission,
+		});
+	} catch (error) {
+		console.error("Error in getting count of the submissions ", error);
+		res.status(500).json({
+			success: false,
+			message: "Error in fetching submission count!",
+			error: error.message,
+		});
+	}
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -19,6 +19,7 @@ app.get("/", (req, res) => {
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/problems", problemRoutes);
 app.use("/api/v1/execute-code", executionRoute);
+app.use("/api/v1/submission", submissionRoutes);
 
 app.listen(process.env.PORT, () => {
 	console.log(`Server is running at PORT: ${process.env.PORT}`);
